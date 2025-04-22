2cd9ec50feeecd53cd57a1bfeafb4dfefd8f9390
Atikam Abhilash Goud <abhigoud8459@gmail.com>
2025-04-22 16:25:26 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Show content (we'll later protect this with CAPTCHA)
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("main-content").style.display = "block";
+});
